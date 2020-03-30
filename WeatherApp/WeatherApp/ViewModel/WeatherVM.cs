fec12b6e10e108edb099977b3c88cef997cc1f32
Patryk Pogorzelczyk <patryk.pogorzelczyk@ordersoft.pl>
2020-03-30 15:24:51 +0200

    Binding Context, Using Design Mode Bindings, Implementing the ICommand interface

--- WeatherApp/WeatherApp/ViewModel/WeatherVM.cs
+++ WeatherApp/WeatherApp/ViewModel/WeatherVM.cs
@@ -4,23 +4,87 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WeatherApp.Model;
+using WeatherApp.ViewModel.Helpers;
 
 namespace WeatherApp.ViewModel
 {
     public class WeatherVM : INotifyPropertyChanged
     {
+        #region Properties
+
+        #region Query
         private string query;
 
         public string Query
         {
             get { return query; }
-            set 
-            { 
+            set
+            {
                 query = value;
                 OnPropertyChanged("Query");
             }
         }
+        #endregion
 
+        #region CurrentConditions
+        private CurrentConditions currentConditions;
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return currentConditions; }
+            set
+            {
+                currentConditions = value;
+                OnPropertyChanged("CurrentConditions");
+            }
+        }
+        #endregion
+
+        #region SelectedCity
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set
+            {
+                selectedCity = value;
+                OnPropertyChanged("SelectedCity");
+            }
+        }
+        #endregion
+
+        #endregion
+
+        public async void MakeQuery()
+        {
+            var cities = await AccuWeatherHelper.GetCities(Query);
+        }
+
+        public WeatherVM()
+        {
+            //check if our project is in Design mode
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                //initialize template data
+                SelectedCity = new City
+                {
+                    LocalizedName = "New York"
+                };
+                CurrentConditions = new CurrentConditions
+                {
+                    WeatherText = "Partly cloudy",
+                    Temperature = new Temperature
+                    {
+                        Metric = new Units
+                        {
+                            Value = 21
+                        }
+                    }
+                };
+            }
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
