91f3ede8a93744d0316acf50bc9a705990379a7f
Patryk Pogorzelczyk <patryk.pogorzelczyk@ordersoft.pl>
2020-03-31 10:16:37 +0200

    ObservableCollection and IValueConverter added

--- WeatherApp/WeatherApp/ViewModel/Helpers/AccuWeatherHelper.cs
+++ WeatherApp/WeatherApp/ViewModel/Helpers/AccuWeatherHelper.cs
@@ -14,7 +14,7 @@ namespace WeatherApp.ViewModel.Helpers
         public const string BASE_URL = "http://dataservice.accuweather.com/";
         public const string AUTOCOMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
         public const string CURRENT_CONDITIONS_ENDPOINT = "currentconditions/v1/{0}?apikey={1}";
-        public const string API_KEY = "CfC4AAC8PJPUBFepApUQgSwXA99ITYM8";
+        public const string API_KEY = "Y9UolKja3oTKUq9B3S9Y7YLZQN39IRZH";
 
         public static async Task<List<City>> GetCities(string query)
         {
--- WeatherApp/WeatherApp/ViewModel/WeatherVM.cs
+++ WeatherApp/WeatherApp/ViewModel/WeatherVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -51,10 +52,17 @@ namespace WeatherApp.ViewModel
             set
             {
                 selectedCity = value;
-                OnPropertyChanged("SelectedCity");
+                if (selectedCity != null)
+                {
+                    OnPropertyChanged("SelectedCity");
+                    GetCurrentConditions();
+                }
             }
         }
         #endregion
+
+        //has to be initialize ONLY ONCE
+        public ObservableCollection<City> Cities { get; set; }
 
         #endregion
 
@@ -62,9 +70,22 @@ namespace WeatherApp.ViewModel
         public SearchCommand SearchCommand { get; set; }
         #endregion
 
+        private async void GetCurrentConditions()
+        {
+            Query = string.Empty;
+            CurrentConditions = await AccuWeatherHelper.GetCurrentConditions(selectedCity.Key);
+        }
+
         public async void MakeQuery()
         {
             var cities = await AccuWeatherHelper.GetCities(Query);
+            //make sure that cities are cleared
+            Cities.Clear();
+            foreach (var city in cities)
+            {
+                //add every single city to observable collection
+                Cities.Add(city);
+            }
         }
 
         public WeatherVM()
@@ -75,7 +96,7 @@ namespace WeatherApp.ViewModel
                 //initialize template data
                 SelectedCity = new City
                 {
-                    LocalizedName = "New York"
+                    //LocalizedName = "New York"
                 };
                 CurrentConditions = new CurrentConditions
                 {
@@ -84,13 +105,14 @@ namespace WeatherApp.ViewModel
                     {
                         Metric = new Units
                         {
-                            Value = 21
+                            Value = "21"
                         }
                     }
                 };
             }
 
             SearchCommand = new SearchCommand(this);
+            Cities = new ObservableCollection<City>();
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
